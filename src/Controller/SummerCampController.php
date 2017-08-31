b41288f3abb3299ce1911867426c0ed05b728b2c
Ryan Weaver <ryan@thatsquality.com>
2017-08-31 22:45:12 +0200

    [tuts-apply-diff] Applying existing diff file for step routes-enable-annotation-import

--- src/Controller/SummerCampController.php
+++ src/Controller/SummerCampController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class SummerCampController extends Controller
@@ -10,6 +11,16 @@ class SummerCampController extends Controller
     {
         return $this->json([
             'fish' => ['brancina', 'tune', 'romba', 'škampi']
+        ]);
+    }
+
+    /**
+     * @Route("/speakers")
+     */
+    public function speakers()
+    {
+        return $this->json([
+            'speakers' => ['Marco', 'Paula', 'Antonio', 'Marcello']
         ]);
     }
 }
