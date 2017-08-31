4dd3c21e837dbb191f8eabc52f00f556226ed8b6
Ryan Weaver <ryan@thatsquality.com>
2017-08-31 22:44:22 +0200

    Step: Profiler: Install profiler

--- config/bundles.php
+++ config/bundles.php
@@ -5,5 +5,6 @@ return [
     'Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle' => ['all' => true],
     'Symfony\Bundle\TwigBundle\TwigBundle' => ['all' => true],
     'Symfony\Bundle\MonologBundle\MonologBundle' => ['all' => true],
+    'Symfony\Bundle\WebProfilerBundle\WebProfilerBundle' => ['dev' => true, 'test' => true],
     'Symfony\Bundle\WebServerBundle\WebServerBundle' => ['dev' => true],
 ];
