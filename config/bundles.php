4bd1f8c1eb761e89eeaa8a3583ad21b5414c0c31
Ryan Weaver <ryan@thatsquality.com>
2017-08-31 22:45:12 +0200

    [tuts-apply-diff] Applying existing diff file for step logger-install-logger

--- config/bundles.php
+++ config/bundles.php
@@ -4,5 +4,6 @@ return [
     'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
     'Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle' => ['all' => true],
     'Symfony\Bundle\TwigBundle\TwigBundle' => ['all' => true],
+    'Symfony\Bundle\MonologBundle\MonologBundle' => ['all' => true],
     'Symfony\Bundle\WebServerBundle\WebServerBundle' => ['dev' => true],
 ];
