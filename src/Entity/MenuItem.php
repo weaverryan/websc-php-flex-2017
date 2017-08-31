3c72d22892b4976f1e2e0d5ade75cd4e563822af
Ryan Weaver <ryan@thatsquality.com>
2017-08-31 22:44:24 +0200

    Step: API: Enable resource

--- src/Entity/MenuItem.php
+++ src/Entity/MenuItem.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="menu_item")
+ * @ApiResource()
  */
 class MenuItem
 {
