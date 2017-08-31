f6c5e3c6bf4b5f221ea96947078e2ee4a36d7014
Ryan Weaver <ryan@thatsquality.com>
2017-08-31 22:44:20 +0200

    Step: Routes: Install annotations + fwextra

--- config/bundles.php
+++ config/bundles.php
@@ -2,5 +2,6 @@
 
 return [
     'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
+    'Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle' => ['all' => true],
     'Symfony\Bundle\WebServerBundle\WebServerBundle' => ['dev' => true],
 ];
