4b91224621affa9c8e28008845bde8bc1c8ca8cf
Ryan Weaver <ryan@thatsquality.com>
2017-08-31 22:44:24 +0200

    Step: API: Install api

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     'Symfony\Bundle\WebServerBundle\WebServerBundle' => ['dev' => true],
     'Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle' => ['all' => true],
     'Doctrine\Bundle\DoctrineBundle\DoctrineBundle' => ['all' => true],
+    'Symfony\Bundle\SecurityBundle\SecurityBundle' => ['all' => true],
+    'ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle' => ['all' => true],
 ];
